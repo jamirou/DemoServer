2241c5236cbe7c244842543fa4eca8af16533b84
jamirou <devjamiro@gmail.com>
2024-04-04 13:57:09 -0300

    Adding a custom header. Check it in postman

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 package com.example
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -8,36 +9,26 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused")
 fun Application.module() {
-    routing {
-        //Way 1
+    install(Routing) {
         get("/") {
-            call.respondText ("Hello, World!")
+            call.respondText("Hello, World!")
         }
         //Way 2
         get("/2") {
-            call.respondText ("This is the path num 2, As you can see, this is another way to add a new path")
+            call.respondText("This is the path num 2, As you can see, this is another way to add a new path")
+        }
+        //Way 3
+        get("/users/{username}") {
+            val username = call.parameters["username"]
+            val header = call.request.headers["connection"]
+            if (username == "Admin") {
+                call.response.header(name = "CustomHeader", value = "Admin")
+                call.respond(message = "Hello $username, You are an administrator", status = HttpStatusCode.OK)
+            }
+            call.respondText("Hello again $username, And your header: $header ")
         }
     }
 }
 
-@Suppress("unused")
-fun Application.module2() {
-    //Way 2 for adding new path
-    routing {
-        get("/3") {
-            call.respondText ("Hello, Android!")
-        }
-    }
-}
 
-//@Suppress("unused")
-//fun Application.module3() {
-//    //Way 4 Adding new path
-//    install(CallLogging) {
-//        routing {
-//            get("/book") {
-//                call.respondText ("Hello, Android!")
-//            }
-//        }
-//    }
-//}
+
